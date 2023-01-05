31ed758ddbfcb691923bbc78a92ae0717a343ca1
ruelperez <karuelflorperez@gmail.com>
2023-01-05 22:51:29 +0800

    save

--- resources/views/scoring.blade.php
+++ resources/views/scoring.blade.php
@@ -10,6 +10,7 @@
             @foreach($criteria as $criterias)
                 <th style="text-align: center;" scope="col">{{ucfirst($criterias->name)}} <br> ({{ucfirst($criterias->percentage)}}%)</p></th>
             @endforeach
+            <th>Total</th>
         </tr>
         </thead>
         <tbody>
@@ -19,7 +20,9 @@
                 <td>photos</td>
                 <td>{{$candidates->fullname}} <br> <p style="font-size: 0.8em";>{{$candidates->address}}</p></td>
                 @foreach($criteria as $criterias)
-                   <td></td>
+                    <form>
+                        <td><input type="text" placeholder="00.00" style="width: 50%; height: 40px; margin-left: 25%; margin-top: 10px; text-align: center"></td>
+                    </form>
                 @endforeach
 
             </tr>
@@ -28,5 +31,6 @@
         </tbody>
     </table>
 </div>
+
 
 @include('partial.footer')
